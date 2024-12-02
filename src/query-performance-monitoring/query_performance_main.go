52c1030f460529a5cef08c8692bae072272fdc72
tharun0064 <tbalanagu@newrelic.com>
2024-12-02 16:03:58 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -2,26 +2,23 @@ package query_performance_monitoring
 
 // this is the main go file for the query_monitoring package
 import (
-	"fmt"
 	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/nri-postgresql/src/args"
-	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-db-connection"
-	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/query_metrics"
 )
 
 func QueryPerformanceMain(instanceEntity *integration.Entity, args args.ArgumentList) {
-	connectionInfo := performance_db_connection.DefaultConnectionInfo(&args)
-	conn, err := connectionInfo.NewConnection(args.Database)
-	if err != nil {
-		fmt.Println("Error in connection")
-	}
+	//connectionInfo := performance_db_connection.DefaultConnectionInfo(&args)
+	//conn, err := connectionInfo.NewConnection(args.Database)
+	//if err != nil {
+	//	fmt.Println("Error in connection")
+	//}
 	metricSet2 := instanceEntity.NewMetricSet("PostgresSlowQueriesV3")
 	metricSet2.SetMetric("test_metric", 10, metric.ATTRIBUTE)
 
-	queryIdList, err := query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn)
-	if err != nil {
-		return
-	}
-	fmt.Println("Query ID List: ", queryIdList)
+	//queryIdList, err := query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn)
+	//if err != nil {
+	//	return
+	//}
+	//fmt.Println("Query ID List: ", queryIdList)
 }
