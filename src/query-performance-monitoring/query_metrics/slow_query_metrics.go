7a854501385adc339165de48f4222872da005fb3
pkudikyala <pkudikyala@newrelic.com>
2024-12-02 12:23:00 +0530

    updated execution plan

--- src/query-performance-monitoring/query_metrics/slow_query_metrics.go
+++ src/query-performance-monitoring/query_metrics/slow_query_metrics.go
@@ -2,6 +2,9 @@ package query_metrics
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
+
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
@@ -10,7 +13,6 @@ import (
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-db-connection"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/queries"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/validations"
-	"reflect"
 )
 
 func GetSlowRunningMetrics(conn *performance_db_connection.PGSQLConnection) ([]datamodels.SlowRunningQuery, []int64, error) {
@@ -21,17 +23,50 @@ func GetSlowRunningMetrics(conn *performance_db_connection.PGSQLConnection) ([]d
 		return nil, nil, err
 	}
 	defer rows.Close()
-	var qIdList []int64
+	var queryInfoList []datamodels.QueryInfo
 	for rows.Next() {
 		var slowQuery datamodels.SlowRunningQuery
 		if err := rows.StructScan(&slowQuery); err != nil {
 			return nil, nil, err
 		}
 		slowQueries = append(slowQueries, slowQuery)
-		qIdList = append(qIdList, *slowQuery.QueryID)
+		// Add QueryID and QueryText to the list
+		queryInfo := datamodels.QueryInfo{
+			QueryID:   *slowQuery.QueryID, // Assuming QueryID is a pointer, adjust if necessary
+			QueryText: slowQuery.QueryText,
+		}
+		queryInfoList = append(queryInfoList, queryInfo)
+		log.Info("Slow Query: %+v", slowQuery)
+		log.Info("Query Info: %+v", queryInfo)
 	}
 
-	return slowQueries, qIdList, nil
+	return slowQueries, queryInfoList, nil
+}
+
+func GetExplainPlanForSlowQueries(conn *performance_db_connection.PGSQLConnection, slowQueries []datamodels.SlowRunningQuery) (map[string]string, error) {
+	explainPlans := make(map[string]string)
+
+	for _, slowQuery := range slowQueries {
+		explainQuery := fmt.Sprintf("EXPLAIN (FORMAT JSON) %s", slowQuery.QueryText)
+		rows, err := conn.Queryx(explainQuery)
+		if err != nil {
+			return nil, err
+		}
+		defer rows.Close()
+
+		var explainResult string
+		for rows.Next() {
+			var row string
+			if err := rows.Scan(&row); err != nil {
+				return nil, err
+			}
+			explainResult += row + "\n"
+		}
+
+		explainPlans[*slowQuery.QueryID] = explainResult
+	}
+
+	return explainPlans, nil
 }
 
 func PopulateSlowRunningMetrics(instanceEntity *integration.Entity, conn *performance_db_connection.PGSQLConnection, args args.ArgumentList) ([]int64, error) {
@@ -57,6 +92,12 @@ func PopulateSlowRunningMetrics(instanceEntity *integration.Entity, conn *perfor
 	}
 	log.Info("Populate-slow running: %+v", slowQueries)
 
+	explainPlans, err := GetExplainPlanForSlowQueries(conn, slowQueries)
+	if err != nil {
+		log.Error("Error fetching explain plans: %v", err)
+		return nil, err
+	}
+
 	for _, model := range slowQueries {
 
 		metricSet := common_utils.CreateMetricSet(instanceEntity, "PostgresSlowQueries", args)
@@ -76,6 +117,7 @@ func PopulateSlowRunningMetrics(instanceEntity *integration.Entity, conn *perfor
 		}
 
 		log.Info("Metrics set for slow query: %s in database: %s", *model.QueryID, *model.DatabaseName)
+		log.Info("Explain plan for query %s: %s", *model.QueryID, explainPlans[*model.QueryID])
 	}
 
 	return queryIdList, nil
