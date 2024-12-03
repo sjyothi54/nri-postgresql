2a40822d3a093c37242edc81a533aabb6d55977f
tharun0064 <tbalanagu@newrelic.com>
2024-12-03 09:22:31 +0530

    test

--- src/query-performance-monitoring/query_metrics/slow_query_metrics.go
+++ src/query-performance-monitoring/query_metrics/slow_query_metrics.go
@@ -26,9 +26,9 @@ func getSlowRunningMetrics(conn *performance_db_connection.PGSQLConnection) ([]d
 		if err := rows.StructScan(&slowQuery); err != nil {
 			return nil, nil, err
 		}
-		if slowQuery.QueryID != nil && *slowQuery.QueryID != 0 {
-			qIdList = append(qIdList, *slowQuery.QueryID)
-		}
+		//if slowQuery.QueryID != nil && *slowQuery.QueryID != 0 {
+		//	qIdList = append(qIdList, *slowQuery.QueryID)
+		//}
 		slowQueries = append(slowQueries, slowQuery)
 	}
 
