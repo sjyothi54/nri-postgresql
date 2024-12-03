39f42ca0e79522a1d939300cda303431f54b7a87
jsurampudi <jsurampudi@newrelic.com>
2024-12-03 19:53:23 +0530

    test

--- src/query_monitoring/query_results/execution_plan.go
+++ src/query_monitoring/query_results/execution_plan.go
@@ -5,27 +5,27 @@ import (
 	"github.com/newrelic/nri-postgresql/src/connection"
 )
 
-func ExecutionPlanQuery(conn *connection.PGSQLConnection) error {
+func ExecutionPlanQuery(conn *connection.PGSQLConnection) {
 	// Prepare the test query
-	rows, err := conn.Queryx("PREPARE test AS SELECT pg_sleep($1)")
+	rows, err := conn.Queryx("PREPARE test AS SELECT pg_sleep($1);")
 	if err != nil {
 		log.Error("Error executing query: ", err.Error())
-		return err
+
 	}
 	defer rows.Close()
-	rows1, err := conn.Queryx("select name from pg_prepared_statements")
+	rows1, err := conn.Queryx("select name from pg_prepared_statements;")
 	if err != nil {
 		log.Error("Error executing query: ", err.Error())
-		return err
+
 	}
 	for rows1.Next() {
 		var name string
 		if err := rows1.Scan(&name); err != nil {
 			log.Error("Error scanning row: ", err.Error())
-			return err
+
 		}
 		log.Info("name: ", name)
 	}
 	defer rows1.Close()
-	return nil
+
 }
