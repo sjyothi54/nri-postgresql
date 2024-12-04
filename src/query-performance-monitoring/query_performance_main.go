3daaebec55dd4b01242bfb9321c5afec2d0bfa67
tharun0064 <tbalanagu@newrelic.com>
2024-12-04 19:48:00 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -23,7 +23,7 @@ func QueryPerformanceMain(instanceEntity *integration.Entity, args args.Argument
 	//}
 	_, err = query_metrics.PopulateIndividualMetrics(instanceEntity, conn, args, nil)
 	if err != nil {
-		fmt.Print("Error in fetching execution plan metrics check:", err)
+		fmt.Print("Error in fetching execution plan metrics check2:", err)
 		return
 	}
 	//query_metrics.PopulateQueryExecutionMetrics(individualMetrics, instanceEntity, conn, args)
