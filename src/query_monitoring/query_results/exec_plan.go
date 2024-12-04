fd5cfcb45cb8242d311eff4f5f9df1eaae126760
jsurampudi <jsurampudi@newrelic.com>
2024-12-04 19:31:30 +0530

    test

--- src/query_monitoring/query_results/exec_plan.go
+++ src/query_monitoring/query_results/exec_plan.go
@@ -7,6 +7,12 @@ import (
 )
 
 func ExecutionPlan(conn *connection.PGSQLConnection) {
+	slowQueries, err := GetSlowRunningMetrics(conn)
+	if err != nil {
+		log.Error("Error fetching slow-running queries: %v", err)
+		return
+	}
+
 	query := queries.ExecutionPlanQuery
 	rows, err := conn.Queryx(query)
 	if err != nil {
@@ -15,6 +21,7 @@ func ExecutionPlan(conn *connection.PGSQLConnection) {
 	}
 	defer rows.Close()
 
+	queryMap := make(map[string]string)
 	for rows.Next() {
 		var queryID string
 		var queryText string
@@ -22,6 +29,19 @@ func ExecutionPlan(conn *connection.PGSQLConnection) {
 			log.Error("Error scanning row: %v", err)
 			return
 		}
-		log.Info("Query ID: %s, Query: %s", queryID, queryText)
+		queryMap[queryID] = queryText
+	}
+
+	for _, slowQuery := range slowQueries {
+		if slowQuery.QueryID == nil {
+			log.Debug("SlowQuery - Query ID is nil")
+			continue
+		}
+		log.Debug("SlowQuery - Query ID: %d", *slowQuery.QueryID)
+		if queryText, exists := queryMap[*slowQuery.QueryID]; exists {
+			log.Info("Matching Query ID: %s, Query: %s", *slowQuery.QueryID, queryText)
+		} else {
+			log.Info("No matching query found for Query ID: %d", *slowQuery.QueryID)
+		}
 	}
 }
