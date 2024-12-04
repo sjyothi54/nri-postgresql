aadf4e622632cd84a8dc4fface43d4040f50c8f0
jsurampudi <jsurampudi@newrelic.com>
2024-12-04 18:47:42 +0530

    test

--- src/main.go
+++ src/main.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
-	"github.com/newrelic/nri-postgresql/src/collection"
 	"github.com/newrelic/nri-postgresql/src/connection"
-	"github.com/newrelic/nri-postgresql/src/inventory"
-	"github.com/newrelic/nri-postgresql/src/metrics"
 	"github.com/newrelic/nri-postgresql/src/query_monitoring"
-	"os"
-	"runtime"
-	"strings"
 )
 
 const (
@@ -36,61 +32,61 @@ func main() {
 		os.Exit(1)
 	}
 
-	if args.ShowVersion {
-		fmt.Printf(
-			"New Relic %s integration Version: %s, Platform: %s, GoVersion: %s, GitCommit: %s, BuildDate: %s\n",
-			strings.Title(strings.Replace(integrationName, "com.newrelic.", "", 1)),
-			integrationVersion,
-			fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
-			runtime.Version(),
-			gitCommit,
-			buildDate)
-		os.Exit(0)
-	}
+	// if args.ShowVersion {
+	// 	fmt.Printf(
+	// 		"New Relic %s integration Version: %s, Platform: %s, GoVersion: %s, GitCommit: %s, BuildDate: %s\n",
+	// 		strings.Title(strings.Replace(integrationName, "com.newrelic.", "", 1)),
+	// 		integrationVersion,
+	// 		fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	// 		runtime.Version(),
+	// 		gitCommit,
+	// 		buildDate)
+	// 	os.Exit(0)
+	// }
 
 	// Setup logging with verbose
-	log.SetupLogging(args.Verbose)
+	// log.SetupLogging(args.Verbose)
 
-	// Validate arguments
-	if err := args.Validate(); err != nil {
-		log.Error("Configuration error for args %v: %s", args, err.Error())
-		os.Exit(1)
-	}
+	// // Validate arguments
+	// if err := args.Validate(); err != nil {
+	// 	log.Error("Configuration error for args %v: %s", args, err.Error())
+	// 	os.Exit(1)
+	// }
 
 	connectionInfo := connection.DefaultConnectionInfo(&args)
-	collectionList, err := collection.BuildCollectionList(args, connectionInfo)
-	if err != nil {
-		log.Error("Error creating list of entities to collect: %s", err)
-		os.Exit(1)
-	}
+	// collectionList, err := collection.BuildCollectionList(args, connectionInfo)
+	// if err != nil {
+	// 	log.Error("Error creating list of entities to collect: %s", err)
+	// 	os.Exit(1)
+	// }
 
 	instance, err := pgIntegration.Entity(fmt.Sprintf("%s:%s", args.Hostname, args.Port), "pg-instance")
-	if err != nil {
-		log.Error("Error creating instance entity: %s", err.Error())
-		os.Exit(1)
-	}
+	// if err != nil {
+	// 	log.Error("Error creating instance entity: %s", err.Error())
+	// 	os.Exit(1)
+	// }
 
-	if args.HasMetrics() {
-		metrics.PopulateMetrics(connectionInfo, collectionList, instance, pgIntegration, args.Pgbouncer, args.CollectDbLockMetrics, args.CollectBloatMetrics, args.CustomMetricsQuery)
-		if args.CustomMetricsConfig != "" {
-			metrics.PopulateCustomMetricsFromFile(connectionInfo, args.CustomMetricsConfig, pgIntegration)
-		}
-	}
+	// if args.HasMetrics() {
+	// 	metrics.PopulateMetrics(connectionInfo, collectionList, instance, pgIntegration, args.Pgbouncer, args.CollectDbLockMetrics, args.CollectBloatMetrics, args.CustomMetricsQuery)
+	// 	if args.CustomMetricsConfig != "" {
+	// 		metrics.PopulateCustomMetricsFromFile(connectionInfo, args.CustomMetricsConfig, pgIntegration)
+	// 	}
+	// }
 
-	if args.HasInventory() {
-		con, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
-		if err != nil {
-			log.Error("Inventory collection failed: error creating connection to PostgreSQL: %s", err.Error())
-		} else {
-			defer con.Close()
-			inventory.PopulateInventory(instance, con)
-		}
-	}
+	// if args.HasInventory() {
+	// 	con, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
+	// 	if err != nil {
+	// 		log.Error("Inventory collection failed: error creating connection to PostgreSQL: %s", err.Error())
+	// 	} else {
+	// 		defer con.Close()
+	// 		inventory.PopulateInventory(instance, con)
+	// 	}
+	// }
 	//need to change the connection to change
 	con, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
 	query_monitoring.RunAnalysis(instance, con, args)
 
-	if err = pgIntegration.Publish(); err != nil {
-		log.Error(err.Error())
-	}
+	// if err = pgIntegration.Publish(); err != nil {
+	// 	log.Error(err.Error())
+	// }
 }
--- src/query_monitoring/queryanalysis.go
+++ src/query_monitoring/queryanalysis.go
@@ -3,14 +3,15 @@ package query_monitoring
 // this is the main go file for the query_monitoring package
 import (
 	"fmt"
+
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/nri-postgresql/src/args"
 	"github.com/newrelic/nri-postgresql/src/connection"
-	"github.com/newrelic/nri-postgresql/src/query_monitoring/queries"
 	"github.com/newrelic/nri-postgresql/src/query_monitoring/query_results"
 )
 
 func RunAnalysis(instanceEntity *integration.Entity, connection *connection.PGSQLConnection, arguments args.ArgumentList) {
-	query_results.PopulateSlowRunningMetrics(instanceEntity, connection, queries.SlowQueries)
+	//query_results.PopulateSlowRunningMetrics(instanceEntity, connection, queries.SlowQueries)
+	query_results.ExecutionPlan(connection)
 	fmt.Println("Query analysis completed.")
 }
