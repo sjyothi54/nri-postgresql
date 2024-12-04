701eec6019f6c2e65d66fe2838c690f5af0f7a38
tharun0064 <tbalanagu@newrelic.com>
2024-12-04 15:48:13 +0530

    test

--- src/query-performance-monitoring/query_metrics/execution_plan_metrics.go
+++ src/query-performance-monitoring/query_metrics/execution_plan_metrics.go
@@ -3,21 +3,29 @@ package query_metrics
 import (
 	"fmt"
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/datamodels"
 	performance_db_connection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-db-connection"
 )
 
-func PopulateQueryExecutionMetrics(queryPlanMetrics []datamodels.QueryPlanMetrics, instanceEntity *integration.Entity, conn *performance_db_connection.PGSQLConnection, args args.ArgumentList) {
+func PopulateQueryExecutionMetrics(queryPlanMetrics []datamodels.QueryPlanMetrics, instanceEntity *integration.Entity, conn *performance_db_connection.PGSQLConnection, args args.ArgumentList) error {
 	for _, queryPlanMetric := range queryPlanMetrics {
 		query := "EXPLAIN (FORMAT JSON) " + queryPlanMetric.QueryText
 		rows, err := conn.Queryx(query)
 		if err != nil {
-			return
+			continue
 		}
 		defer rows.Close()
-		for rows.Next() {
-			fmt.Print("rowsss:", rows)
+		if !rows.Next() {
+			return nil
 		}
+		var execPlanJSON string
+		if err := rows.Scan(&execPlanJSON); err != nil {
+			log.Error("Error scanning row: ", err.Error())
+			continue
+		}
+		fmt.Print("execPlanJSON:", execPlanJSON)
 	}
+	return nil
 }
