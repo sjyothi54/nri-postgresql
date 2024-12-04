9d9cde949f2a979363a47654fea03f314a324d97
jsurampudi <jsurampudi@newrelic.com>
2024-12-04 19:27:50 +0530

    test

--- src/query_monitoring/query_results/exec_plan.go
+++ src/query_monitoring/query_results/exec_plan.go
@@ -7,12 +7,6 @@ import (
 )
 
 func ExecutionPlan(conn *connection.PGSQLConnection) {
-	slowQueries, err := GetSlowRunningMetrics(conn)
-	if err != nil {
-		log.Error("Error fetching slow-running queries: %v", err)
-		return
-	}
-
 	query := queries.ExecutionPlanQuery
 	rows, err := conn.Queryx(query)
 	if err != nil {
@@ -21,7 +15,6 @@ func ExecutionPlan(conn *connection.PGSQLConnection) {
 	}
 	defer rows.Close()
 
-	queryMap := make(map[string]string)
 	for rows.Next() {
 		var queryID string
 		var queryText string
@@ -29,44 +22,6 @@ func ExecutionPlan(conn *connection.PGSQLConnection) {
 			log.Error("Error scanning row: %v", err)
 			return
 		}
-		queryMap[queryID] = queryText
-	}
-
-	// Fetch query IDs and texts from pg_stat_monitor
-	monitorQuery := queries.ExecutionPlanMonitorQuery
-	monitorRows, err := conn.Queryx(monitorQuery)
-	if err != nil {
-		log.Error("Error executing monitor query: %v", err)
-		return
-	}
-	defer monitorRows.Close()
-
-	for monitorRows.Next() {
-		var queryID string
-		var queryText string
-		if err := monitorRows.Scan(&queryID, &queryText); err != nil {
-			log.Error("Error scanning monitor row: %v", err)
-			return
-		}
-		queryMap[queryID] = queryText
-	}
-
-	// Debug log to print queryMap
-	for queryID, queryText := range queryMap {
-		log.Debug("QueryMap - Query ID: %s, Query: %s", queryID, queryText)
-	}
-
-	for _, slowQuery := range slowQueries {
-		// Debug log to print slowQuery.QueryID
-		if slowQuery.QueryID == nil {
-			log.Debug("SlowQuery - Query ID is nil")
-			continue
-		}
-		log.Debug("SlowQuery - Query ID: %d", *slowQuery.QueryID)
-		if queryText, exists := queryMap[*slowQuery.QueryID]; exists {
-			log.Info("Matching Query ID: %s, Query: %s", *slowQuery.QueryID, queryText)
-		} else {
-			log.Info("No matching query found for Query ID: %d", *slowQuery.QueryID)
-		}
+		log.Info("Query ID: %s, Query: %s", queryID, queryText)
 	}
 }
