a2364505b2474dbd00febb2451baffb8d7152daa
tharun0064 <tbalanagu@newrelic.com>
2024-12-06 11:46:31 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -19,11 +19,11 @@ func QueryPerformanceMain(instanceEntity *integration.Entity, args args.Argument
 		fmt.Print("Error in connection")
 		return
 	}
-	//queryIdList, err := query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn, args)
-	//if err != nil {
-	//	fmt.Printf("Error in fetching slow running metrics: %v", err)
-	//	return
-	//}
+	_, err = query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn, args)
+	if err != nil {
+		fmt.Printf("Error in fetching slow running metrics: %v", err)
+		return
+	}
 
 	ms := common_utils.CreateMetricSet(instanceEntity, "PostgresqlWaitEventMetricsV1Test", args)
 	err = ms.SetMetric("event_type", "PostgresqlWaitEventMetricsV1Test", metric.ATTRIBUTE)
