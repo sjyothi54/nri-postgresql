62a45022da337136505658b2809a96995e2bc929
tharun0064 <tbalanagu@newrelic.com>
2024-12-06 11:38:55 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -3,8 +3,11 @@ package query_performance_monitoring
 // this is the main go file for the query_monitoring package
 import (
 	"fmt"
+	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
+	common_utils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-db-connection"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/query_metrics"
 )
@@ -21,6 +24,14 @@ func QueryPerformanceMain(instanceEntity *integration.Entity, args args.Argument
 	//	fmt.Printf("Error in fetching slow running metrics: %v", err)
 	//	return
 	//}
+
+	ms := common_utils.CreateMetricSet(instanceEntity, "PostgresqlWaitEventMetricsV1Test", args)
+	err = ms.SetMetric("event_type", "PostgresqlWaitEventMetricsV1Test", metric.ATTRIBUTE)
+	if err != nil {
+		log.Error("Error setting event_type attribute: %v", err)
+		return
+	}
+
 	err = query_metrics.PopulateWaitEventMetrics(instanceEntity, conn, args)
 	if err != nil {
 		fmt.Printf("Error in fetching wait event metrics: %v\n", err)
