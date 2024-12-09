5c083a3dc085cd590197036547842e0262db14a7
tharun0064 <tbalanagu@newrelic.com>
2024-12-09 11:58:57 +0530

    test

--- src/query-performance-monitoring/common-utils/helpers.go
+++ src/query-performance-monitoring/common-utils/helpers.go
@@ -32,26 +32,26 @@ func metricSet(e *integration.Entity, eventType, hostname string, port string) *
 func SetMetric(metricSet *metric.Set, name string, value interface{}, sourceType string) {
 	switch sourceType {
 	case `gauge`:
-		var numericValue float64
-		switch v := value.(type) {
-		case int:
-			numericValue, _ = castToFloat(v)
-		case int64:
-			numericValue, _ = castToFloat(v)
-		case float64:
-			numericValue = v
-		default:
-			fmt.Println("Error: gauge metric requires a numeric value")
-			return
-		}
-		fmt.Println("Numeric value: ", numericValue)
-		err := metricSet.SetMetric(name, numericValue, metric.GAUGE)
+		//var numericValue float64
+		//switch v := value.(type) {
+		//case int:
+		//	numericValue, _ = castToFloat(v)
+		//case int64:
+		//	numericValue, _ = castToFloat(v)
+		//case float64:
+		//	numericValue = v
+		//default:
+		//	fmt.Println("Error: gauge metric requires a numeric value")
+		//	return
+		//}
+		//fmt.Println("Numeric value: ", numericValue)
+		err := metricSet.SetMetric(name, value, metric.GAUGE)
 		if err != nil {
 			fmt.Println("Error in setting metric1", err)
 			return
 		}
 	case `attribute`:
-		err := metricSet.SetMetric(name, fmt.Sprintf("%v", value), metric.ATTRIBUTE)
+		err := metricSet.SetMetric(name, value, metric.ATTRIBUTE)
 		if err != nil {
 			fmt.Println("Error in setting metric", err)
 			return
