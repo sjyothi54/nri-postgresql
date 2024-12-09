336506814a0c0160cc18c1fbd5d9897adcbc2aa1
tharun0064 <tbalanagu@newrelic.com>
2024-12-09 11:16:45 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -9,21 +9,20 @@ import (
 	"github.com/newrelic/nri-postgresql/src/args"
 	common_utils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-db-connection"
-	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/query_metrics"
 )
 
 func QueryPerformanceMain(instanceEntity *integration.Entity, args args.ArgumentList) {
 	connectionInfo := performance_db_connection.DefaultConnectionInfo(&args)
-	conn, err := connectionInfo.NewConnection(args.Database)
+	_, err := connectionInfo.NewConnection(args.Database)
 	if err != nil {
 		fmt.Print("Error in connection")
 		return
 	}
-	_, err = query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn, args)
-	if err != nil {
-		fmt.Printf("Error in fetching slow running metrics: %v", err)
-		return
-	}
+	//_, err = query_metrics.PopulateSlowRunningMetrics(instanceEntity, conn, args)
+	//if err != nil {
+	//	fmt.Printf("Error in fetching slow running metrics: %v", err)
+	//	return
+	//}
 
 	ms := common_utils.CreateMetricSet(instanceEntity, "PostgresqlGoWaitEventMetrics", args)
 	err = ms.SetMetric("event_type", "PostgresqlWaitEventMetricsV1Test", metric.ATTRIBUTE)
