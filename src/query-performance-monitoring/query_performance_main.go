a16411607712a3f9b332fd47a08c13e7e2206e24
tharun0064 <tbalanagu@newrelic.com>
2024-12-30 13:48:47 +0530

    test

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -2,7 +2,7 @@ package query_performance_monitoring
 
 // this is the main go file for the query_monitoring package
 import (
-	"fmt"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	performanceDbConnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
 	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/validations"
 
@@ -15,7 +15,7 @@ func QueryPerformanceMain(args args.ArgumentList, pgIntegration *integration.Int
 	connectionInfo := performanceDbConnection.DefaultConnectionInfo(&args)
 	newConnection, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
 	if err != nil {
-		fmt.Println("Error creating connection: ", err)
+		log.Info("Error creating connection: ", err)
 		return
 	}
 
@@ -26,7 +26,7 @@ func QueryPerformanceMain(args args.ArgumentList, pgIntegration *integration.Int
 	performance_metrics.PopulateBlockingMetrics(pgIntegration, args)
 	individualQueries := performance_metrics.PopulateIndividualQueryMetrics(slowRunningQueries, pgIntegration, args)
 	performance_metrics.PopulateExecutionPlanMetrics(individualQueries, pgIntegration, args)
-	fmt.Println("Query analysis completed.")
+	log.Info("Query analysis completed.")
 }
 
 func loadConfiguration(args args.ArgumentList, conn *performanceDbConnection.PGSQLConnection) {
