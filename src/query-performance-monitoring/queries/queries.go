1fbfe0b7356744156fa794263e0fc90dacebd6d4
tharun0064 <tbalanagu@newrelic.com>
2024-12-30 17:45:36 +0530

    test

--- src/query-performance-monitoring/queries/queries.go
+++ src/query-performance-monitoring/queries/queries.go
@@ -108,7 +108,7 @@ const (
 			FROM
 				pg_stat_monitor
 			WHERE
-				queryid IN (%s) AND ((total_exec_time / NULLIF(calls, 0))::numeric  > (%d))
+				queryid IN (%s) 
 			GROUP BY
 				query, queryid, datname, planid, cpu_user_time, cpu_sys_time, calls `
 
