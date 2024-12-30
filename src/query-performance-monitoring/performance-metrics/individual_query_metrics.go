46d25ef729080592feb9e18f3243a8ce2e48a25b
tharun0064 <tbalanagu@newrelic.com>
2024-12-30 19:58:22 +0530

    Query performance monitoring
    Query performance monitoring
    
    undo change
    
    remove test query
    
    query performance monitoring
    
    undo changelog changes
    
    Query performance monitoring
    
    Query performance monitoring
    
    slow query len check in individual queries metrics populate
    
    trim query text
    
    lint issue fix
    
    undo changelog file change

---
+++ src/query-performance-monitoring/performance-metrics/individual_query_metrics.go
@@ -0,0 +1,99 @@
+package performancemetrics
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	"github.com/newrelic/nri-postgresql/src/args"
+	commonutils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
+	performancedbconnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
+	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/datamodels"
+	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/queries"
+	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/validations"
+)
+
+func PopulateIndividualQueryMetrics(conn *performancedbconnection.PGSQLConnection, slowRunningQueries []datamodels.SlowRunningQueryMetrics, pgIntegration *integration.Integration, args args.ArgumentList) []datamodels.IndividualQueryMetrics {
+	isExtensionEnabled, err := validations.CheckIndividualQueryMetricsFetchEligibility(conn)
+	if err != nil {
+		log.Error("Error executing query: %v", err)
+		return nil
+	}
+	if !isExtensionEnabled {
+		log.Info("Extension 'pg_stat_monitor' is not enabled.")
+		return nil
+	}
+	log.Info("Extension 'pg_stat_monitor' enabled.")
+	individualQueryMetricsInterface, individualQueriesForExecPlan := GetIndividualQueryMetrics(conn, slowRunningQueries, args)
+	if len(individualQueryMetricsInterface) == 0 {
+		log.Info("No individual queries found.")
+		return nil
+	}
+	commonutils.IngestMetric(individualQueryMetricsInterface, "PostgresIndividualQueries", pgIntegration, args)
+	return individualQueriesForExecPlan
+}
+
+func ConstructIndividualQuery(slowRunningQueries []datamodels.SlowRunningQueryMetrics, args args.ArgumentList) string {
+	var queryIDs = make([]string, 0)
+	for _, query := range slowRunningQueries {
+		queryIDs = append(queryIDs, fmt.Sprintf("%d", *query.QueryID))
+	}
+	query := fmt.Sprintf(queries.IndividualQuerySearch, strings.Join(queryIDs, ","))
+	return query
+}
+
+func GetIndividualQueryMetrics(conn *performancedbconnection.PGSQLConnection, slowRunningQueries []datamodels.SlowRunningQueryMetrics, args args.ArgumentList) ([]interface{}, []datamodels.IndividualQueryMetrics) {
+	if len(slowRunningQueries) == 0 {
+		log.Info("No slow running queries found.")
+		return nil, nil
+	}
+	query := ConstructIndividualQuery(slowRunningQueries, args)
+	log.Info("Individual query :", query)
+	rows, err := conn.Queryx(query)
+	if err != nil {
+		log.Info("Error executing query in individual query: %v", err)
+		return nil, nil
+	}
+	anonymizedQueriesByDB := processForAnonymizeQueryMap(slowRunningQueries)
+	var individualQueryMetricsForExecPlanList []datamodels.IndividualQueryMetrics
+	var individualQueryMetricsListInterface []interface{}
+	for rows.Next() {
+		var model datamodels.IndividualQueryMetrics
+		if scanErr := rows.StructScan(&model); scanErr != nil {
+			log.Error("Could not scan row: ", scanErr)
+			continue
+		}
+		individualQueryMetric := model
+		anonymizedQueryText := anonymizedQueriesByDB[*model.DatabaseName][*model.QueryID]
+		individualQueryMetric.QueryText = &anonymizedQueryText
+		generatedPlanID := commonutils.GenerateRandomIntegerString(*model.QueryID)
+		individualQueryMetric.PlanID = generatedPlanID
+		model.PlanID = generatedPlanID
+		model.RealQueryText = model.QueryText
+		model.QueryText = &anonymizedQueryText
+
+		individualQueryMetricsForExecPlanList = append(individualQueryMetricsForExecPlanList, model)
+		individualQueryMetricsListInterface = append(individualQueryMetricsListInterface, individualQueryMetric)
+	}
+	if closeErr := rows.Close(); closeErr != nil {
+		log.Error("Error closing rows: %v", closeErr)
+	}
+	return individualQueryMetricsListInterface, individualQueryMetricsForExecPlanList
+}
+
+func processForAnonymizeQueryMap(queryCPUMetricsList []datamodels.SlowRunningQueryMetrics) map[string]map[int64]string {
+	anonymizeQueryMapByDB := make(map[string]map[int64]string)
+
+	for _, metric := range queryCPUMetricsList {
+		dbName := *metric.DatabaseName
+		queryID := *metric.QueryID
+		anonymizedQuery := *metric.QueryText
+
+		if _, exists := anonymizeQueryMapByDB[dbName]; !exists {
+			anonymizeQueryMapByDB[dbName] = make(map[int64]string)
+		}
+		anonymizeQueryMapByDB[dbName][queryID] = anonymizedQuery
+	}
+	return anonymizeQueryMapByDB
+}
---
+++ src/query-performance-monitoring/query_performance_main.go
@@ -0,0 +1,49 @@
+package queryperformancemonitoring
+
+// this is the main go file for the query_monitoring package
+import (
+	"time"
+
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
+	performancedbconnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
+
+	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/nri-postgresql/src/args"
+	performancemetrics "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-metrics"
+)
+
+func QueryPerformanceMain(args args.ArgumentList, pgIntegration *integration.Integration) {
+	connectionInfo := performancedbconnection.DefaultConnectionInfo(&args)
+	newConnection, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
+	if err != nil {
+		log.Info("Error creating connection: ", err)
+		return
+	}
+
+	start := time.Now()
+	log.Info("Starting PopulateSlowRunningMetrics at ", start)
+	slowRunningQueries := performancemetrics.PopulateSlowRunningMetrics(newConnection, pgIntegration, args)
+	log.Info("PopulateSlowRunningMetrics completed in ", time.Since(start))
+
+	start = time.Now()
+	log.Info("Starting PopulateWaitEventMetrics at ", start)
+	performancemetrics.PopulateWaitEventMetrics(newConnection, pgIntegration, args)
+	log.Info("PopulateWaitEventMetrics completed in ", time.Since(start))
+
+	start = time.Now()
+	log.Info("Starting PopulateBlockingMetrics at ", start)
+	performancemetrics.PopulateBlockingMetrics(newConnection, pgIntegration, args)
+	log.Info("PopulateBlockingMetrics completed in ", time.Since(start))
+
+	start = time.Now()
+	log.Info("Starting PopulateIndividualQueryMetrics at ", start)
+	individualQueries := performancemetrics.PopulateIndividualQueryMetrics(newConnection, slowRunningQueries, pgIntegration, args)
+	log.Info("PopulateIndividualQueryMetrics completed in ", time.Since(start))
+
+	start = time.Now()
+	log.Info("Starting PopulateExecutionPlanMetrics at ", start)
+	performancemetrics.PopulateExecutionPlanMetrics(individualQueries, pgIntegration, args)
+	log.Info("PopulateExecutionPlanMetrics completed in ", time.Since(start))
+
+	log.Info("Query analysis completed.")
+}
