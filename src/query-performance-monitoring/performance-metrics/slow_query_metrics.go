d9b7216580a07fe49067c177e53494744893303e
tharun0064 <tbalanagu@newrelic.com>
2025-01-06 15:56:41 +0530

    test

--- src/query-performance-monitoring/performance-metrics/slow_query_metrics.go
+++ src/query-performance-monitoring/performance-metrics/slow_query_metrics.go
@@ -17,7 +17,6 @@ func GetSlowRunningMetrics(conn *performancedbconnection.PGSQLConnection, args a
 	var slowQueryMetricsListInterface []interface{}
 	versionSpecificQuery, err := commonutils.FetchVersionSpecificSlowQueries(conn)
 	var query = fmt.Sprintf(versionSpecificQuery, databaseNames, min(args.QueryCountThreshold, commonutils.MAX_QUERY_THRESHOLD))
-	log.Info("SLOW Query: ", query)
 	rows, err := conn.Queryx(query)
 	if err != nil {
 		return nil, nil, err
