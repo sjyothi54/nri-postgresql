c99d07da55a89114ece279f0980c66f6e33284a8
tharun0064 <tbalanagu@newrelic.com>
2025-01-06 11:58:44 +0530

    version specific queries

--- src/query-performance-monitoring/common-utils/ingestion-helpers.go
+++ src/query-performance-monitoring/common-utils/ingestion-helpers.go
@@ -3,19 +3,14 @@ package commonutils
 import (
 	"crypto/rand"
 	"fmt"
-	"math/big"
-	"reflect"
-	"regexp"
-	"strconv"
-	"time"
-
 	_ "github.com/lib/pq"
 	"github.com/newrelic/infra-integrations-sdk/v3/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
 	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
-	performancedbconnection "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/connections"
-	"github.com/newrelic/nri-postgresql/src/query-performance-monitoring/queries"
+	"math/big"
+	"reflect"
+	"time"
 )
 
 const publishThreshold = 100
@@ -117,6 +112,7 @@ func publishMetrics(pgIntegration *integration.Integration, instanceEntity **int
 	*instanceEntity, err = pgIntegration.Entity(fmt.Sprintf("%s:%s", args.Hostname, args.Port), "pg-instance")
 	return err
 }
+
 func GenerateRandomIntegerString(queryID int64) *string {
 	randomInt, err := rand.Int(rand.Reader, big.NewInt(randomIntRange))
 	if err != nil {
@@ -125,68 +121,4 @@ func GenerateRandomIntegerString(queryID int64) *string {
 	currentTime := time.Now().Format("20060102150405")
 	result := fmt.Sprintf("%d-%d-%s", queryID, randomInt.Int64(), currentTime)
 	return &result
-}
-
-func FetchVersion(conn *performancedbconnection.PGSQLConnection) (int, error) {
-	var versionStr string
-	rows, err := conn.Queryx("SELECT version()")
-	if err != nil {
-		log.Error("Error executing query: %v", err)
-		return 0, err
-	}
-	defer rows.Close()
-
-	if rows.Next() {
-		if err := rows.Scan(&versionStr); err != nil {
-			log.Error("Error scanning version: %v", err)
-			return 0, err
-		}
-	}
-
-	log.Info("versionstring", versionStr)
-
-	re := regexp.MustCompile(`PostgreSQL (\d+)\.`)
-	matches := re.FindStringSubmatch(versionStr)
-	if len(matches) < 2 {
-		log.Error("Unable to parse PostgreSQL version from string: %s", versionStr)
-		return 0, fmt.Errorf("unable to parse PostgreSQL version from string: %s", versionStr)
-	}
-
-	version, err := strconv.Atoi(matches[1])
-	log.Info("version", version)
-	if err != nil {
-		log.Error("Error converting version to integer: %v", err)
-		return 0, err
-	}
-	return version, nil
-}
-
-func FetchVersionSpecificSlowQueries(conn *performancedbconnection.PGSQLConnection) (string, error) {
-	version, err := FetchVersion(conn)
-	if err != nil {
-		return "", err
-	}
-	switch {
-	case version == 12:
-		return queries.SlowQueriesForV12, nil
-	case version >= 13:
-		return queries.SlowQueriesForV13AndAbove, nil
-	default:
-		return "", fmt.Errorf("unsupported PostgreSQL version: %d", version)
-	}
-}
-
-func FetchVersionSpecificBlockingQueries(conn *performancedbconnection.PGSQLConnection) (string, error) {
-	version, err := FetchVersion(conn)
-	if err != nil {
-		return "", err
-	}
-	switch {
-	case version == 12, version == 13:
-		return queries.BlockingQueriesForV12AndV13, nil
-	case version >= 14:
-		return queries.BlockingQueriesForV14AndAbove, nil
-	default:
-		return "", fmt.Errorf("unsupported PostgreSQL version: %d", version)
-	}
 }
