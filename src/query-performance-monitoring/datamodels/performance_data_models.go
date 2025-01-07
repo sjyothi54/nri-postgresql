0b0ec1301eb2c3e122f75b97d14365d0d5b2c137
tharun0064 <tbalanagu@newrelic.com>
2025-01-07 14:37:43 +0530

    test

--- src/query-performance-monitoring/datamodels/performance_data_models.go
+++ src/query-performance-monitoring/datamodels/performance_data_models.go
@@ -1,7 +1,7 @@
 package datamodels
 
 type SlowRunningQueryMetrics struct {
-	Newrelic            *string  `db:"newrelic"              metric_name:"newrelic"                   source_type:"attribute"  ingestData:"false"`
+	Newrelic            *string  `db:"newrelic"              metric_name:"newrelic"                   source_type:"attribute"  ingest_data:"false"`
 	QueryID             *int64   `db:"query_id"              metric_name:"query_id"                   source_type:"attribute"`
 	QueryText           *string  `db:"query_text"            metric_name:"query_text"                 source_type:"attribute"`
 	DatabaseName        *string  `db:"database_name"         metric_name:"database_name"              source_type:"attribute"`
@@ -24,7 +24,7 @@ type WaitEventMetrics struct {
 	DatabaseName        *string  `db:"database_name"         metric_name:"database_name"              source_type:"attribute"`
 }
 type BlockingSessionMetrics struct {
-	Newrelic           *string `db:"newrelic"              metric_name:"newrelic"            source_type:"attribute"  ingestData:"false"`
+	Newrelic           *string `db:"newrelic"              metric_name:"newrelic"            source_type:"attribute"  ingest_data:"false"`
 	BlockedPid         *int64  `db:"blocked_pid"          metric_name:"blocked_pid"          source_type:"gauge"`
 	BlockedQuery       *string `db:"blocked_query"        metric_name:"blocked_query"        source_type:"attribute"`
 	BlockedQueryID     *string `db:"blocked_query_id"     metric_name:"blocked_query_id"     source_type:"attribute"`
@@ -42,9 +42,9 @@ type IndividualQueryMetrics struct {
 	DatabaseName    *string  `json:"datname" db:"datname" metric_name:"database_name" source_type:"attribute"`
 	AvgCPUTimeInMS  *float64 `json:"avg_cpu_time_ms" db:"avg_cpu_time_ms" metric_name:"avg_cpu_time_ms" source_type:"gauge"`
 	PlanID          *string  `json:"planid" db:"planid" metric_name:"plan_id" source_type:"attribute"`
-	RealQueryText   *string  `ingestData:"false"`
-	AvgExecTimeInMs *float64 `json:"avg_exec_time_ms" db:"avg_exec_time_ms" ingestData:"false"`
-	Newrelic        *string  `db:"newrelic"              metric_name:"newrelic"            source_type:"attribute"  ingestData:"false"`
+	RealQueryText   *string  `ingest_data:"false"`
+	AvgExecTimeInMs *float64 `json:"avg_exec_time_ms" db:"avg_exec_time_ms" ingest_data:"false"`
+	Newrelic        *string  `db:"newrelic"              metric_name:"newrelic"            source_type:"attribute"  ingest_data:"false"`
 }
 
 type QueryExecutionPlanMetrics struct {
