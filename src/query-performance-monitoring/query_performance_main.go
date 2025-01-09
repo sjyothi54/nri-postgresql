5b67ced4b9df354953571db6aa93890be5ab5dc7
tharun0064 <tbalanagu@newrelic.com>
2025-01-09 18:21:41 +0530

    fix

--- src/query-performance-monitoring/query_performance_main.go
+++ src/query-performance-monitoring/query_performance_main.go
@@ -2,17 +2,15 @@ package queryperformancemonitoring
 
 // this is the main go file for the query_monitoring package
 import (
-	"github.com/newrelic/nri-postgresql/src/metrics"
 	"time"
 
-	"github.com/newrelic/nri-postgresql/src/collection"
-	commonutils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
-
-	"github.com/newrelic/infra-integrations-sdk/v3/log"
-	performancedbconnection "github.com/newrelic/nri-postgresql/src/connection"
-
 	"github.com/newrelic/infra-integrations-sdk/v3/integration"
+	"github.com/newrelic/infra-integrations-sdk/v3/log"
 	"github.com/newrelic/nri-postgresql/src/args"
+	"github.com/newrelic/nri-postgresql/src/collection"
+	performancedbconnection "github.com/newrelic/nri-postgresql/src/connection"
+	"github.com/newrelic/nri-postgresql/src/metrics"
+	commonutils "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/common-utils"
 	performancemetrics "github.com/newrelic/nri-postgresql/src/query-performance-monitoring/performance-metrics"
 )
 
@@ -23,17 +21,20 @@ func QueryPerformanceMain(args args.ArgumentList, pgIntegration *integration.Int
 		log.Debug("No databases found")
 		return
 	}
+
 	newConnection, err := connectionInfo.NewConnection(connectionInfo.DatabaseName())
 	if err != nil {
 		log.Debug("Error creating connection: ", err)
 		return
 	}
+
 	version, versionErr := metrics.CollectVersion(newConnection)
 	versionInt := version.Major
 	if versionErr != nil {
 		log.Debug("Error fetching version: ", versionErr)
 		return
 	}
+
 	start := time.Now()
 	log.Debug("Starting PopulateSlowRunningMetrics at ", start)
 	slowRunningQueries := performancemetrics.PopulateSlowRunningMetrics(newConnection, pgIntegration, args, databaseStringList, versionInt)
