bc2e421c7774b549f6ee4fcbcf18a29690ace73c
tharun0064 <tbalanagu@newrelic.com>
2025-05-21 11:18:48 +0530

    feat : is_rds flag fetch from install framework

--- tests/postgresqlperf_test.go
+++ tests/postgresqlperf_test.go
@@ -92,10 +92,24 @@ func TestIntegrationWithDatabaseLoadPerfEnabled(t *testing.T) {
 			args:       []string{`-collection_list=all`},
 		},
 		{
-			name: "Performance metrics collection test with query moniotoring only flag enabled",
+			name:                "Performance metrics collection test with query moniotoring only flag enabled",
 			expectedSampleTypes: newSampleTypes,
-			containers: perfContainers,
-			args:       []string{`-collection_list=all`, `-query_monitoring_only=true`},
+			containers:          perfContainers,
+			args:                []string{`-collection_list=all`, `-query_monitoring_only=true`},
+		},
+		{
+			name:                "Performance metrics collection test with enable query monitoring flag enabled and is_rds flag enabled",
+			expectedSampleTypes: allSampleTypes,
+			containers:          perfContainers,
+			args:                []string{`-collection_list=all`, `-enable_query_monitoring=true`, `-is_rds=true`},
+		},
+		{
+			name:                "Performance metrics collection test with enable query monitoring flag enabled and is_rds flag disabled",
+			expectedSampleTypes: []string{
+				"PostgresqlInstanceSample",
+			},
+			containers:          perfContainers,
+			args:                []string{`-collection_list=all`, `-enable_query_monitoring=true`, `-is_rds=false`},
 		},
 	}
 
